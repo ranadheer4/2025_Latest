6d5f923447ef6c024641213663785312b84ff6a6
ranadheer <dranadheer4@gmail.com>
2025-06-18 19:28:52 +0530

    day3_LoopsStrings

---
+++ LoopsNo'sStrings/whileloopdemo.py
@@ -0,0 +1,17 @@
+# while loop
+
+# print 1.....10 numbers
+
+# i=1  #initilization
+# while i<=10:     #1 2 3  4 5 6 7 9 10
+#     print(i)    #1  2 3 4 5 6 8 9 10
+#     i=i+1    #2 3 4 5 6 7 9 10 11
+# print("Done!!!!")
+
+
+# print 1...10 numbers in descending order (10,9,8,7,.....1)
+i=10
+while i>=1:
+    print(i) #10 9 8 7 6 5 4 3 2 1
+    i=i-1  #9  8 7 6 5 4 3 2 1 0
+print("Done!!!")
